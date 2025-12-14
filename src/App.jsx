1e0177c50ce7b46392859987dee79d53174a964e
Eyuel-Lakew <42708498+Eyuel-Lakew@users.noreply.github.com>
2025-12-14 11:43:54 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-import Navigation from "./component/navigation";
+import Navigation from "./component/Navigation";
 import Picture from "./component/Picture";
 import Footer from "./component/Footer";
 
